938e7567b854b40d85491f07074cce9e07608d90
pavel11123 <korobko.11@inbox.ru>
2022-08-08 22:48:16 +0600

    add sweetalert for form

--- src/components/ContactUsModal.jsx
+++ src/components/ContactUsModal.jsx
@@ -1,39 +1,81 @@
-import React, { useRef } from 'react';
-import emailjs from '@emailjs/browser';
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
+import Swal from "sweetalert2";
 
 const ContactUsModal = () => {
-    const form = useRef();
+  const form = useRef();
 
-    const sendEmail = (e) => {
-        e.preventDefault();
+  const sendEmail = (e) => {
+    e.preventDefault();
 
-        emailjs.sendForm('service_48u2ccd', 'template_i91omvq', form.current, 'eewGStA5uCmwJrG3X')
-            .then((result) => {
-                console.log(result.text);
-            }, (error) => {
-                console.log(error.text);
-            });
-    };
+    emailjs
+      .sendForm(
+        "service_48u2ccd",
+        "template_i91omvq",
+        form.current,
+        "eewGStA5uCmwJrG3X"
+      )
+      .then(
+        () => {
+          Swal.fire({
+            title: "Заявка отправлена!",
+            text: "Скоро мы свяжемся с Вами",
+            icon: "success",
+            background: "white",
+            allowEnterKey: false,
+            confirmButtonColor: "#f8b700",
+            timer: 3000,
+          });
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
 
-    return (
-        <form ref={form} onSubmit={sendEmail}>
-            <div className="modal-main">
-                <input type="text" name="client_name" placeholder="Ваше имя"
-                    class="feedback__name feedback__input" />
-                <input type="text" name="client_city"
-                    placeholder="Ваш город"
-                    class="feedback__city feedback__input" />
-                <input type="text" name="client_phone"
-                    placeholder="Ваш телефон"
-                    class="feedback__phone feedback__input" />
-                <input type="text" name="client_address" placeholder="Точный адрес"
-                    class="feedback__address feedback__input" />
-                <input type="text" name="client_value" placeholder="Объём"
-                    class="feedback__volume feedback__input" />
-                <input class="feedback__submit feedback__input" type="submit" value="Отправить" />
-            </div>
-        </form>
-    );
+  return (
+    <form ref={form} onSubmit={sendEmail}>
+      <div className="modal-main">
+        <input
+          type="text"
+          name="client_name"
+          placeholder="Ваше имя"
+          class="feedback__name feedback__input"
+          required=" "
+        />
+        <input
+          type="text"
+          name="client_city"
+          placeholder="Ваш город"
+          class="feedback__city feedback__input"
+        />
+        <input
+          type="text"
+          name="client_phone"
+          placeholder="Ваш телефон"
+          class="feedback__phone feedback__input"
+          required=" "
+        />
+        <input
+          type="text"
+          name="client_address"
+          placeholder="Точный адрес"
+          class="feedback__address feedback__input"
+        />
+        <input
+          type="text"
+          name="client_value"
+          placeholder="Объём"
+          class="feedback__volume feedback__input"
+        />
+        <input
+          class="feedback__submit feedback__input"
+          type="submit"
+          value="Отправить"
+        />
+      </div>
+    </form>
+  );
 };
 
 export default ContactUsModal;
--- src/components/ContactUsSmall.jsx
+++ src/components/ContactUsSmall.jsx
@@ -1,34 +1,65 @@
-import React, { useRef } from 'react';
-import emailjs from '@emailjs/browser';
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
+import Swal from "sweetalert2";
 
 const ContactUsSmall = () => {
-    const form = useRef();
+  const form = useRef();
 
-    const sendEmail = (e) => {
-        e.preventDefault();
+  const sendEmail = (e) => {
+    e.preventDefault();
 
-        emailjs.sendForm('service_48u2ccd', 'template_i91omvq', form.current, 'eewGStA5uCmwJrG3X')
-            .then((result) => {
-                console.log(result.text);
-            }, (error) => {
-                console.log(error.text);
-            });
-    };
+    emailjs
+      .sendForm(
+        "service_48u2ccd",
+        "template_i91omvq",
+        form.current,
+        "eewGStA5uCmwJrG3X"
+      )
+      .then(
+        () => {
+          Swal.fire({
+            title: "Заявка отправлена!",
+            text: "Скоро мы свяжемся с Вами",
+            icon: "success",
+            background: "white",
+            allowEnterKey: false,
+            confirmButtonColor: "#f8b700",
+            timer: 3000,
+          });
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
 
-    return (
-        <form ref={form} onSubmit={sendEmail}>
-            <div className="form-main">
-                <input type="text" name="client_name" placeholder="Ваше имя"
-                    class="feedback__name feedback__input" />
+  return (
+    <form ref={form} onSubmit={sendEmail}>
+      <div className="form-main">
+        <input
+          type="text"
+          name="client_name"
+          placeholder="Ваше имя"
+          class="feedback__name feedback__input"
+          required=" "
+        />
 
-                <input type="text" name="client_phone"
-                    placeholder="Ваш телефон"
-                    class="feedback__phone feedback__input" />
+        <input
+          type="text"
+          name="client_phone"
+          placeholder="Ваш телефон"
+          class="feedback__phone feedback__input"
+          required=" "
+        />
 
-                <input class="feedback__submit feedback__input" type="submit" value="Отправить" />
-            </div>
-        </form>
-    );
+        <input
+          class="feedback__submit feedback__input"
+          type="submit"
+          value="Отправить"
+        />
+      </div>
+    </form>
+  );
 };
 
 export default ContactUsSmall;
